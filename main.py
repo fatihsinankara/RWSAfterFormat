1227116a9f96b9cf54d2cd74de6ef5e6a4a0cf99
fatihsinankara <fatihskara@gmail.com>
2022-07-08 11:57:14 +0300

    Temel program listesi eklendi.

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import wget
 
-file_url = input("Enter the URL of the file to download: ")
-file_name = wget.download(file_url)
+output_directory = "dlcache"
+
+welcome = """
+    #####################################################
+    # Merhaba.                                          #
+    # RWS After Format programına hoşgeldiniz.          #
+    # Lütfen kurmak istediğiniz programın numarasını    #
+    # yazıp 'Enter' tuşuna basın.                       #
+    #####################################################
+"""
+programlist = """
+    Tarayıcılar:
+    (1) Google Chrome
+    (2) Opera
+    (3) Vivaldi
+    (4) Firefox
+
+    Virüs Programları:
+    (5) Avast Free
+    (6) AVG Free
+
+    Diğer:
+    (7) Winrar
+    (8) 7-Zip
+    (9) Steam
+    (10) Discord
+
+"""
+print (welcome)
+print (programlist)
+secim = input("Lütfen kurmak istediğiniz programı tuşlayınız: ")
